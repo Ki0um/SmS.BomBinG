5096d0b96b5ff615de32315fe06697009badcdfd
Ki0um <101413887+Ki0um@users.noreply.github.com>
2022-11-19 15:24:43 +0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -21,9 +21,9 @@ $(document).ready(() => {
                     url: "http://robi.api.bongobd.com/api/login/send-otp",
                     body: `msisdn=88${mobile}&operator=all`
                 },
-                {
-                    method: 'GET',
-                    url: "https://sms.ignoredhaxor.xyz/api/v1/sms/single?channel=0xApp&number={mobile}&message={amount}"
+
+
+
                 {
                     method: 'GET',
                     url: `http://45.114.85.19:8080/v3/otp/send?msisdn=88${mobile}`
